f7c94cdc9af40ab481cd849719a3a6b0bc770d97
David Bush <david.bush@process-intellect.com>
2016-08-10 19:53:45 -0500

    backed off release

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -8,7 +8,7 @@ read -p "Really deploy to maven cetral repository  (yes/no)? "
 if ( [ "$REPLY" == "yes" ] ) then
   ssh-add ~/.ssh/id_rsa.pub
   ssh-add -l
-  mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
+  mvn -Dmaven.test.skip=true release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
 else
   echo 'Exit without deploy'
